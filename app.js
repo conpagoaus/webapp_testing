53ef8903b79d9992f08fcf3f4699c03814953b60
Maxim Filimonov <tpaktopsp@gmail.com>
2023-06-06 17:05:15 +0800

    add req logging

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ app.get('/test', (req, res) => {
     res.send('Test Route!');
 })
 
+// log all requests headers and url to console
+app.use((req, res, next) => {
+    console.log(new Date().toUTCString());
+    console.log(req.headers);
+    console.log(req.url);
+    console.log(req.method);
+    next();
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 })

da1879c3782fac26b0e15f7b7c02a49e517f7aa9
Maxim Filimonov <tpaktopsp@gmail.com>
2023-06-07 14:22:21 +0800

    add logs

--- extract_repo_ref.sh
+++ extract_repo_ref.sh
@@ -2,6 +2,7 @@
 # example script frontend=1,backend=2 frontend
 # will return 1
 # if no frontend is found will return main
+echo "Processing $1"
 
 HEAD=$(echo "$1" | tr "," "\n" | grep "$2" | cut -d "=" -f 2)
 # if nothing re default to main

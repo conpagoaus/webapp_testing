0fc2665fab4e292bc2f83edd2ed212e49d991655
Maxim Filimonov <tpaktopsp@gmail.com>
2023-06-07 14:25:10 +0800

    fix bug

--- extract_repo_ref.sh
+++ extract_repo_ref.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 # example script frontend=1,backend=2 frontend
 # will return 1
-# if no frontend is found will return main
-echo "Processing $1"
 
+# if only one argument is passed return main
+if [ $# -eq 1 ]
+then
+    echo "main"
+    exit 0
+fi
+
+
+
+# if no frontend is found will return main
 HEAD=$(echo "$1" | tr "," "\n" | grep "$2" | cut -d "=" -f 2)
 # if nothing re default to main
 # else return head
